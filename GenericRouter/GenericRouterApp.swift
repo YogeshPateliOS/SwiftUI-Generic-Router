58956208cb65b8f25133853e5e055a8c55f105f8
hemalpatelios <134166548+hemalpatelios@users.noreply.github.com>
2025-03-07 20:32:35 -0500

    Initial Commit

---
+++ GenericRouter/GenericRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericRouterApp.swift
+//  GenericRouter
+//
+//  Created by Yogesh Patel on 2025-03-07.
+//
+
+import SwiftUI
+
+@main
+struct GenericRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
